810c6d1dcdca1cfa0b6f5512185e95f816585291
dheerapat <dheerapat.t@kkumail.com>
2022-05-13 13:59:23 +0700

    initial commit finished site

---
+++ main.js
@@ -0,0 +1,33 @@
+const lower = 'abcdefghijklmnopqrstuvwxyz';
+const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+const special = '!@#$%()_*+-';
+
+const strings = lower + upper + nums + special;
+//console.log(strings)
+let password = ""
+//console.log(typeof (password))
+
+const generatePass = () => {
+    let len = 16;
+    let ind = 0;
+    var arr = [];
+
+    for (i = 0; i < len; i++) {
+        ind = Math.floor(Math.random() * strings.length);
+        arr.push(strings[ind]);
+        password = arr.join('');
+    };
+    pw.innerHTML = password;
+    copy.style.visibility = 'visible';
+};
+
+let button = document.getElementById('button');
+let pw = document.getElementById('pw');
+let copy = document.getElementById('copy');
+
+button.onclick = generatePass;
+copy.onclick = function () {
+    let copied = document.getElementById('pw').innerHTML;
+    navigator.clipboard.writeText(copied);
+};
